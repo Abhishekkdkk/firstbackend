337f555aba18d3fa084c53bca1b36c404e06904f
Abhishekkdkk <abhishekkdk.2022@gmail.com>
2025-07-20 06:16:38 +0545

    Initial commit

---
+++ src/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    watchHistory: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Video'
+    }],
+    refreshToken: {
+        type: String,
+        default: null
+    },
+    avatar: {
+        type: String,
+    }
+    
+    
+},{timestamps: true});
+userSchema.pre('save', function(next) {
+    if (!this.isModified('password')) {
+       return next();
+    }
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password);//RETURNS TRUE OR FALSE
+}
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign({ _id: this._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY });
+}
+const User = mongoose.model("User", userSchema);
+export default User;
